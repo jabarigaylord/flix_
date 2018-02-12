2fdbd44292e32abc907a9f5406272e56b0f9a06e
Gaylord <gaylord@oprc-3022.morgan.edu>
2018-02-12 15:30:54 -0800

    Initial Commit

---
+++ flix_/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix_
+//
+//  Created by Gaylord on 2/12/18.
+//  Copyright © 2018 Gaylord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
